f09b3833a1782bcf31f454643dedff6e2dac85ef
João César Marigonda <joao@webnow.com.br>
2015-10-16 14:10:16 -0300

    Removendo CorreiosDAO (nao faz sentido DAO para o correio)

--- compre-facil-correios/src/main/java/br/unicamp/comprefacil/dao/CorreiosDAO.java
+++
@@ -1,8 +0,0 @@
-package br.unicamp.comprefacil.dao;
-
-import br.unicamp.comprefacil.model.Frete;
-
-public interface CorreiosDAO {
-	public Frete calculateShipping(int peso, int largura, int altura, int comprimento, String tipoEntrega, String cep) throws Throwable;
-
-}
--- compre-facil-correios/src/main/java/br/unicamp/comprefacil/services/CorreiosServices.java
+++ compre-facil-correios/src/main/java/br/unicamp/comprefacil/services/CorreiosServices.java
@@ -1,7 +1,13 @@
 package br.unicamp.comprefacil.services;
 
-import br.unicamp.comprefacil.dao.CorreiosDAO;
-import br.unicamp.comprefacil.dao.CorreiosDAOImpl;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.http.ResponseDefinition;
+
 import br.unicamp.comprefacil.dao.DadosDeEntregaDAO;
 import br.unicamp.comprefacil.model.Endereco;
 import br.unicamp.comprefacil.model.Frete;
@@ -34,11 +40,49 @@ public class CorreiosServices {
 	public Frete calculaFrete(Pedido p) throws Throwable {
 		if(!isOn())
 			throw new Throwable("CorreioServices is offline right now");
-		
-		CorreiosDAO correiosDAO = new CorreiosDAOImpl();
-		Frete f = correiosDAO.calculateShipping(p.getPeso(), p.getLargura(), p.getAltura(), p.getComprimento(), p.getTipoEntrega(), p.getCep());
+
+		Frete f = calculateShipping(p.getPeso(), p.getLargura(), p.getAltura(), p.getComprimento(), p.getTipoEntrega(), p.getCep());
 		dao.saveDadosDeEntrega(f.getValor(), f.getTempoEntrega());
 		return f;
+	}
+	
+	private Frete calculateShipping(int peso, int largura, int altura,
+			int comprimento, String tipoEntrega, String cep) throws Throwable{
+
+		Frete frete = null;
+		if (cep.equalsIgnoreCase("04119010")) {
+			
+			int valor = 0;
+			int tempoEntrega = 0;
+			if(tipoEntrega.equals("PAC") && cep.equals("04119010")){
+				valor = 5;
+				tempoEntrega = 5;
+			} else if(tipoEntrega.equals("SEDEX") && cep.equals("04119010")){
+				valor = 20;
+				tempoEntrega = 3;
+			} else if(tipoEntrega.equals("SEDEX10") && cep.equals("04119010")){
+				valor = 100;
+				tempoEntrega = 1;
+			}
+			
+			ResponseDefinitionBuilder builder = aResponse()
+	                .withHeader("Content-Type", "text/plain")
+	                .withBody("{\"Valor\":\""+valor+"\",\"PrazoEntrega\":\""+tempoEntrega+"\"}");
+			ResponseDefinition resp = builder.build();
+			
+			JSONObject json = (JSONObject) new JSONParser().parse(resp.getBody());
+			
+			String valorResp = json.get("Valor").toString();
+			String tempoEntregaResp = json.get("PrazoEntrega").toString();
+			
+			frete = new Frete();
+			frete.setTempoEntrega(Integer.valueOf(tempoEntregaResp));
+			frete.setValor(Double.valueOf(valorResp));
+		} else {
+			throw new Throwable("The zipcode is not valid");
+	    }
+
+		return frete;
 	}
 
 	public boolean isOn() {
